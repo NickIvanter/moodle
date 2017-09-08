d28b58448031597f8edc5a4330223ca0caac487a
nonamenerd <nonamenerd@gmail.com>
2017-09-08 17:54:59 +0500

    Setting lang (#109)

--- filter/cloudfront_hlsaes/lang/en/filter_cloudfront_hlsaes.php
+++ filter/cloudfront_hlsaes/lang/en/filter_cloudfront_hlsaes.php
@@ -6,18 +6,9 @@
  * @subpackage cloudfront_hlsaes
  */
 
-$string['maindistr'] = 'HLS AES distribution';
-$string['maindistrdesc'] = 'The cloudfront name of the web  distribution e.g. d111111abcdef8';
-$string['filtername'] = 'Cloudfront HLS AES';
-$string['videowidth'] = 'Video width';
-$string['videowidthdesc'] = 'Default width of video player';
-$string['videoheight'] = 'Video height';
-$string['videoheightdesc'] = 'Default height of video player';
-$string['videoautostart'] = 'Video autostart';
-$string['videoautostartdesc'] = 'Autostart playback on page load';
-$string['videothumb'] = 'Video thumb';
-$string['videothumbdesc'] = 'Create video thumbnail. This will autoload video!';
-$string['playerkey'] = 'License key for player';
-$string['playerkeydesc'] = '';
-$string['aeskeys'] = 'JSON Video id map';
-$string['aeskeysdesc'] = '';
+$string['tokenurl'] = 'Video Key Token URL';
+$string['tokenurldesc'] = 'URL for acquiring token to key service';
+$string['tokensecret'] = 'Token Service Secret';
+$string['tokensecretdesc'] = 'Secret string for access to token service';
+$string['keyurl'] = 'Video Key URL';
+$string['keyurldesc'] = 'URL for acquiring video decryption key';
